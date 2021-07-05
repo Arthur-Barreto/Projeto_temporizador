09b25e6b182757fa792ac159202ef14497c71663
Arthur-Barreto <arthurbarreto2905@gmail.com>
2021-07-04 21:33:06 -0300

    atualização

--- new_blink_ds3231/new_blink_ds3231.ino
+++ new_blink_ds3231/new_blink_ds3231.ino
@@ -28,18 +28,21 @@
 #define bomba2 5
 
 RTC_DS3231 rtc; //OBJETO DO TIPO RTC_DS3231
-int h,m,s;
-bool um,dois;
+
+char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+
+int h;
+bool um;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(bomba1,OUTPUT);
-  pinMode(bomba2,OUTPUT);
+  //pinMode(bomba2,OUTPUT);
   digitalWrite(bomba1,LOW);
-  digitalWrite(bomba2,LOW);
+  //digitalWrite(bomba2,LOW);
   um = false;
-  dois = false;
+  //dois = false;
 
 } // end setup
 
@@ -48,18 +51,16 @@ void loop() {
   DateTime now = rtc.now(); //CHAMADA DE FUNÇÃO
 
   h = now.hour();
-  m = now.minute();
-  s = now.second();
-
+  
   switch (h){
-    case 17:
+    case 5:
       um = true;
       break;
     default:
       um = false;
       break;
   } // end switch bomba 1
-
+/*
   switch (m){
     case 2:
       dois = true;
@@ -74,8 +75,25 @@ void loop() {
       dois = false;
       break;
   } // end switch bomba 2
-  
+  */
   digitalWrite(bomba1,um);
-  digitalWrite(bomba2,dois);
+  //digitalWrite(bomba2,dois);
+
+  Serial.print("Data: "); //IMPRIME O TEXTO NO MONITOR SERIAL
+  Serial.print(now.day(), DEC); //IMPRIME NO MONITOR SERIAL O DIA
+  Serial.print('/'); //IMPRIME O CARACTERE NO MONITOR SERIAL
+  Serial.print(now.month(), DEC); //IMPRIME NO MONITOR SERIAL O MÊS
+  Serial.print('/'); //IMPRIME O CARACTERE NO MONITOR SERIAL
+  Serial.print(now.year(), DEC); //IMPRIME NO MONITOR SERIAL O ANO
+  Serial.print(" / Dia: "); //IMPRIME O TEXTO NA SERIAL
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]); //IMPRIME NO MONITOR SERIAL O DIA
+  Serial.print(" / Horas: "); //IMPRIME O TEXTO NA SERIAL
+  Serial.print(now.hour(), DEC); //IMPRIME NO MONITOR SERIAL A HORA
+  Serial.print(':'); //IMPRIME O CARACTERE NO MONITOR SERIAL
+  Serial.print(now.minute(), DEC); //IMPRIME NO MONITOR SERIAL OS MINUTOS
+  Serial.print(':'); //IMPRIME O CARACTERE NO MONITOR SERIAL
+  Serial.print(now.second(), DEC); //IMPRIME NO MONITOR SERIAL OS SEGUNDOS
+  Serial.println(); //QUEBRA DE LINHA NA SERIAL
+  delay(1000); //INTERVALO DE 1 SEGUNDO
   
 } // end loop
